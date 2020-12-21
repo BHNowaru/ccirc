c231432290935d754f326983d80c62cf270b1e03
BHNowaru <zackyboy135@gmail.com>
2020-12-21 16:45:34 -0700

    update import

--- importer.lua
+++ importer.lua
@@ -7,29 +7,55 @@ local names = {
 if (callingArguments[1] == "-s") then-- -s for select
     callingArguments = {table.unpack(callingArguments, 2)}
     for i, v in pairs(callingArguments) do
-        if (table.find(names, tostring(i):lower())) then
+        if (names[i:lower()]) then
+            if (fs.exists(i..".lua")) then
+                print("Attempting to remove",i..".lua...");
+                local success = shell.run("rm", i..".lua");
+                if (success) then
+                    term.setTextColour(term.isColour and colours.lime or colours.white);
+                    print("Successfully removed!")
+                    term.setTextColour(colours.white);
+                else
+                    term.setTextColour(term.isColour and colours.red or colours.lightGray);
+                    print("Could not remove", tostring(i)..".lua.")
+                    term.setTextColour(colours.white);
+                end
+            end
             local success = shell.run("wget", "https://raw.githubusercontent.com/BHNowaru/ccirc/main/" .. i .. ".lua", i..".lua")
             if (not success) then
-                term.setTextColour(term.isColour and colours.lime or colours.white);
+                term.setTextColour(term.isColour and colours.red or colours.lightGray);
                 print("Could not install", tostring(i)..".lua.")
                 term.setTextColour(colours.white);
             else
-                term.setTextColour(term.isColour and colours.red or colours.lightGray);
-                print("Successfully installed")
+                term.setTextColour(term.isColour and colours.lime or colours.white);
+                print("Successfully installed!")
                 term.setTextColour(colours.white);
             end
         end
     end
 else
     for i, v in pairs(names) do
-        local success = shell.run("wget", "https://raw.githubusercontent.com/BHNowaru/ccirc/main/" .. i .. ".lua", i ..".lua")
+        if (fs.exists(i..".lua")) then
+            print("Attempting to remove",i..".lua...");
+            local success = shell.run("rm", i..".lua");
+            if (success) then
+                term.setTextColour(term.isColour and colours.lime or colours.white);
+                print("Successfully removed!")
+                term.setTextColour(colours.white);
+            else
+                term.setTextColour(term.isColour and colours.red or colours.lightGray);
+                print("Could not remove", tostring(i)..".lua.")
+                term.setTextColour(colours.white);
+            end
+        end
+        local success = shell.run("wget", "https://raw.githubusercontent.com/BHNowaru/ccirc/main/" .. i .. ".lua", i..".lua")
         if (not success) then
-            term.setTextColour(term.isColour and colours.lime or colours.white);
+            term.setTextColour(term.isColour and colours.red or colours.lightGray);
             print("Could not install", tostring(i)..".lua.")
             term.setTextColour(colours.white);
         else
-            term.setTextColour(term.isColour and colours.red or colours.lightGray);
-            print("Successfully installed")
+            term.setTextColour(term.isColour and colours.lime or colours.white);
+            print("Successfully installed!")
             term.setTextColour(colours.white);
         end
     end
